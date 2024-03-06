e037a3901915103c6cceb0f73b941715145c3241
eviva2000 <arjomandielaheh@gamin.com>
2024-03-06 11:40:00 +0100

    updated footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import Canvas from "./components/canvas/Canvas";
+import Channels from "./components/Channels";
 function App() {
   return (
     <div className="App">
@@ -8,21 +9,12 @@ function App() {
         <div className="brand">
           <h3>Elaheh Arjomandi</h3>
         </div>
-        <div className="channels">
-          <a href="https://github.com/eviva2000">
-            <img src="/github.svg" alt="" />
-          </a>
-          <a href="https://www.linkedin.com/in/elaheharjomandi/">
-            <img src="/linkedin.svg" alt="" />
-          </a>
-          <a href="mailto:arjomandielaheh@gmail.com">
-            <img src="/email.svg" alt="" />
-          </a>
-        </div>
+        <Channels />
       </header>
       <Canvas />
       <footer>
-        <p>Made with 💙 by Elaheh </p> <p>© 2024</p>
+        <Channels />
+        <p>© 2024</p>
       </footer>
     </div>
   );
---
+++ src/components/Channels.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Channels = () => {
+  return (
+    <div className="channels">
+      <a href="https://github.com/eviva2000">
+        <img src="/github.svg" alt="" />
+      </a>
+      <a href="https://www.linkedin.com/in/elaheharjomandi/">
+        <img src="/linkedin.svg" alt="" />
+      </a>
+      <a href="mailto:arjomandielaheh@gmail.com">
+        <img src="/email.svg" alt="" />
+      </a>
+    </div>
+  );
+};
+
+export default Channels;
